ce06cd19732735eb9127f55b4f40fe0f2f11d6ef
sharon3snc <sharonnunez89@gmail.com>
2023-03-02 21:26:56 +0100

    redux

---
+++ src/Email.jsx
@@ -0,0 +1,14 @@
+import {useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
+import {changeEmail} from "./redux/userSlice";
+
+export function Email() {
+    const email = useSelector((state)=>state.user.email)
+    const dispatch = useDispatch();
+    
+    return(
+        <input type="email" 
+        value={email} 
+        onChange={(event)=> dispatch(changeEmail(event.target.value))} />
+    )
+}
